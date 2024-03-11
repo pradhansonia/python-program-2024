0af759c5e9f2c18505c707928ee991bc066b874d
[Sonia Pradhan] <[03230316.gcbs@rub.edu.bt]>
2024-03-11 12:59:53 +0530

    first commit

---
+++ dataTypes.py
@@ -0,0 +1,25 @@
+x = "Hello World" #String
+x = 50  #integer
+x = 60.5 
+x = 3j
+x = ["greeks", "for", "greeks"]
+x = range(10)
+x = {"name": "Suraj", "age":24}
+x = {"greeks","for","greeks"}
+x = frozenset({"greeks", "for", "greeks"})
+x = True
+x = b"Greeks"
+x = bytearray(4)
+x = memoryview(bytes(6))
+x = None
+
+
+
+my_List = [1,2,3,4,5, 'String']
+my_List.insert(1,5) #insert 5 at a specific index
+print(my_List)
+
+my_List.append(5)
+print(my_List)
+
+print
